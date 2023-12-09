eccacbea20f9dc82a1a5fc74d6fa3a1c30437447
kayes360 <imrulkayes560@gmail.com>
2023-12-10 02:49:10 +0600

    first commit

---
+++ src/components/videos/Videos.jsx
@@ -0,0 +1,34 @@
+import Video from "./Video";
+import VideoLoader from "../ui/loaders/VideoLoader";
+import Error from "../ui/Error";
+import { useGetVideosQuery } from "../../features/api/apiSlice";
+
+export default function Videos() {
+  const { data: videos, isLoading, isError } = useGetVideosQuery();
+
+  //decide what to render
+
+  let content = null;
+
+  if (isLoading) {
+    content = <VideoLoader />;
+  }
+
+  if (!isLoading && isError) {
+    content = <Error message="There was an error"/>;
+  }
+
+  if (!isLoading && !isError && videos?.length === 0) {
+    content = <Error  message="No video found!"/>;
+  }
+
+  if (!isLoading && !isError && videos?.length > 0) {
+    content = videos.map(video => <Video key={video.id} video={video}/>);
+  }
+
+  return (
+    <>
+      {content}
+    </>
+  );
+}
